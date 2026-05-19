ce46f9def55834df1b44fdde61d2ab8684bab1c0
Carolina Figueiredo <carolinasfigueiredo@hotmail.com>
2026-05-19 00:47:27 -0300

    Atualizaçao auth controller

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -5,15 +5,15 @@ import { UsuarioLogin } from './../entities/usuariologin.entity';
 import { ApiTags } from '@nestjs/swagger/dist/decorators/api-use-tags.decorator';
 import { ApiBody } from '@nestjs/swagger/dist/decorators/api-body.decorator';
 
-@ApiTags('Usuarios')
-@Controller("/usuarios")
+@ApiTags('auth')
+@Controller("/auth")
 export class AuthController {
     constructor(private authService: AuthService) { }
 
     @UseGuards(LocalAuthGuard)
     @HttpCode(HttpStatus.OK)
     @ApiBody({ type: UsuarioLogin })
-    @Post('/logar')
+    @Post('/login')
     login(@Request() req): Promise<any> {
         return this.authService.login(req.user);
     }
